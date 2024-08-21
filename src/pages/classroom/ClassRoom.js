5e803b1c510dc9ca9b6e93ca95d20601fbec69cb
JoEunsang <sang11032@gmail.com>
2024-08-21 13:53:10 +0900

    [feat] 강의 종료 시, 시선 데이터 저장 기능 추가

--- src/pages/classroom/ClassRoom.js
+++ src/pages/classroom/ClassRoom.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import SideBar from "../main/SideBar";
 import TopBar from "../main/TopBar";
 import './css/ClassRoom.css';
@@ -9,6 +9,54 @@ const ClassRoom = () => {
     const [files, setFiles] = useState([]);
     const [alertMessage, setAlertMessage] = useState('');
 
+    const [data, setData] = useState();
+    const [dataArray, setDataArray] = useState([]);
+
+    const dataRef = useRef([]);
+
+    useEffect(() => {
+        if (window.electron && window.electron.tobiiJarOutput) {
+            window.electron.tobiiJarOutput((output) => {
+                setData(output);
+                const parsedData = parseJson(output);
+                dataRef.current.push(parsedData);
+            });
+        } else {
+            setData(['웹에서는 동작 안해요']);
+        }
+    }, []);
+
+    function parseJson(data) {
+        const dataArray = data.split(',');
+        const parsedObject = {
+            xRatio: parseFloat(dataArray[0]),
+            yRatio: parseFloat(dataArray[1]),
+            xPosition: parseInt(dataArray[2], 10),
+            yPosition: parseInt(dataArray[3], 10)
+        };
+
+        return parsedObject;
+    }
+    const sendEyeData = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/send/message', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(dataRef.current),
+            });
+            if (response.ok) {
+                console.log('데이터 전송 성공');
+                dataRef.current = [];
+                setDataArray([]);
+            } else {
+                console.error('데이터 전송 중 오류 발생', dataRef.current);
+            }
+        } catch (error) {
+            console.error('데이터 전송 중 오류 발생', error);
+        }
+    };
     useEffect(() => {
         // SSE 연결 설정
         const eventSource = new EventSource('http://220.149.128.13:8080/alert');
@@ -16,10 +64,18 @@ const ClassRoom = () => {
         eventSource.onopen = () => {
             console.log("SSE-Connect")
         }
-        eventSource.onmessage = (event) =>{
-            console.log(event.data)
-            setAlertMessage(event.data)
-        }
+        eventSource.onmessage = async (event) => {
+            console.log(event.data);
+            setAlertMessage(event.data);
+
+            if (event.data === "exit") {
+                try {
+                    await sendEyeData();
+                } catch (error) {
+                    console.error('sendEyeData failed:', error);
+                }
+            }
+        };
         eventSource.onerror = (event) => {
             eventSource.close();
             if (event.target.readyState === EventSource.CLOSED) {
